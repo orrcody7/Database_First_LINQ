c89e8184c333b4ccb500a8da00f391e28c0f38a7
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-09-23 14:55:05 -0400

    Completed problem 4

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -18,7 +18,7 @@ namespace DatabaseFirstLINQ
             ProblemOne();
             ProblemTwo();
             ProblemThree();
-            //ProblemFour();
+            ProblemFour();
             //ProblemFive();
             //ProblemSix();
             //ProblemSeven();
@@ -76,7 +76,12 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that gets each product that contains an "s" in the products name.
             // Then print the name of each product from the above query to the console.
-
+            var products = _context.Products;
+            var productNameThatContainsAnS = products.Where(p => p.Name.Contains("s"));
+            foreach (var product in productNameThatContainsAnS)
+            {
+                Console.WriteLine(product.Name);
+            }
         }
 
         private void ProblemFive()
